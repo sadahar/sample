33dd8b70c3b80b2aca752cf3bdb287084e8ef825
sadahar <sadaharlogin@gmail.com>
2022-08-13 19:07:45 +0530

    adding the project

---
+++ src/main/java/configuration/Confi.java
@@ -0,0 +1,19 @@
+package configuration;
+
+public class Confi {
+	
+	public static String WebdriverForChrome="webdriver.chrome.driver";
+	public  static String WebdriverForEdge="webdriver.edge.driver";
+	
+	
+	public static String driverpathChrome="C:\\Users\\BusinessComputers.in\\eclipse-workspaceSADA\\HealthCareDomainProject\\src\\main\\resources\\Driver\\chromedriver.exe";
+    public static String driverpathEdge ="C:\\Users\\BusinessComputers.in\\eclipse-workspaceSADA\\HealthCareDomainProject\\src\\main\\resources\\Driver\\msedgedriver.exe";
+    
+    public static String UrlKite="https://kite.zerodha.com";
+    
+    public static String ExcelpathFbdata="C:\\Users\\BusinessComputers.in\\eclipse-workspaceSADA\\HealthCareDomainProject\\src\\test\\resources\\TestData\\FbData.xlsx";
+    public static String ExcelpathMatrimo="C:\\Users\\BusinessComputers.in\\eclipse-workspaceSADA\\HealthCareDomainProject\\src\\test\\resources\\TestData\\MatrimonialData.xlsx";
+
+    
+    public static String Screenshot="C:\\Users\\BusinessComputers.in\\eclipse-workspaceSADA\\HealthCareDomainProject\\target\\Screenshot\\"+".jpg";
+}
